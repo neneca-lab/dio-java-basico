9f67f6370ddd87262b7e7131583f4ad636a0a134
huan <huansonecacntt@gmail.com>
2024-04-29 09:36:20 -0300

    xo maldicao

---
+++ project-java/src/Soneca.java
@@ -0,0 +1,8 @@
+public class Soneca {
+    
+    public static void main(String[] args){
+        System.out.println("Hello, World!!");
+        //out é do tipo saida e print é impressão, logo apos 
+    }
+
+}
