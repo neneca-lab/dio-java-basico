a30190809372e6d73431c9d174cd2b81182e2b51
huan <huansonecacntt@gmail.com>
2024-05-01 18:32:09 -0300

    declaracao e metodos

--- project-java/src/Soneca.java
+++ project-java/src/Soneca.java
@@ -1,8 +1,14 @@
 public class Soneca {
     
     public static void main(String[] args){
-        System.out.println("Hello, World!!");
-        //out é do tipo saida e print é impressão, logo apos 
+        String primeiroNome = "Huan";
+        String segundoNome = "Salomao";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
     }
 
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "Resultado do método nomeCompleto: " +  primeiroNome.concat(" ").concat(segundoNome);
+    }
 }
